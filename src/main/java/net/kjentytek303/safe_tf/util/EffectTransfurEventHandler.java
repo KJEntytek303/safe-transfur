0e406e564fef9348c82855903d3f37dfb709dfbc
KJEntytek303 <jakdom6@gmail.com>
2026-06-05 17:24:10 +0200

    broke serum handlers (further)

--- src/main/java/net/kjentytek303/safe_tf/util/EffectTransfurEventHandler.java
+++ src/main/java/net/kjentytek303/safe_tf/util/EffectTransfurEventHandler.java
@@ -12,6 +12,7 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.network.PacketDistributor;
@@ -26,54 +27,48 @@ import static net.kjentytek303.safe_tf.init.InitEffects.*;
 
 
 public class EffectTransfurEventHandler {
-	@SubscribeEvent
-	public static void onTransfur(ProcessTransfur.KeepConsciousEvent event) {
-		if (event.shouldKeepConscious || event.player == null) {return;}
+	@SubscribeEvent( priority = EventPriority.HIGH )
+	public static void onPlayerTransfur(ProcessTransfur.KeepConsciousEvent event) {
+		//if already safe tfed or not a player
+		if (event.keepConscious || event.shouldKeepConscious || event.player == null) {return;}
 
-		if (event.player.getEffect(UNSAFE_SERUM.get()) == null && event.player.getEffect(SUSPICIOUS_SERUM.get()) == null) {
+		//if doesn't have the effect, return.
+		if (! ( event.player.hasEffect(UNSAFE_SERUM.get()) || event.player.hasEffect(SUSPICIOUS_SERUM.get()) ) ) {
 			return;
 		}
 
-		//Player must have the effect.
+		//Player must have the effect. If CAddon isn't loaded, handle it normally.
+		if (event.player.level().isClientSide()) { return; }
+
 		if (!ModList.get().isLoaded("changed_addon") ) {
 			event.shouldKeepConscious = true;
 			return;
 		}
 
 
-		event.shouldKeepConscious = handleCaddonCfg(event);
-		if (event.shouldKeepConscious) {
-			return;
-		}
+		//CAddon compat start
+		event.shouldKeepConscious = handleCaddonCompatCfg(event);
+		if (event.shouldKeepConscious) { return; }
 
-		//CAddon is loaded
-		//Thx foxyas for help.
-		Level level = event.player.level();
-		if (event.player instanceof ServerPlayer player) {
-
-			ChangedAddonVariables.PlayerVariables vars1 = ChangedAddonVariables.ofOrDefault(player);
-			FightToKeepConsciousness.MinigameType minigameType = FightToKeepConsciousness.MinigameType.getRandom(player.getRandom());
-			vars1.isTransfuredBySafeMethod = false;
-			updatePlayerVariables(vars1, minigameType, 0, player);
-			ChangedAddonMod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> player), new ClientboundOpenFTKCScreenPacket(minigameType));
+		boolean ftkc = event.player.level().getGameRules().getBoolean(ChangedAddonGameRules.FIGHT_TO_KEEP_CONSCIOUSNESS);
+		if (event.player instanceof ServerPlayer player ) {
 			event.shouldKeepConscious = true;
+			ChangedAddonVariables.PlayerVariables vars = ChangedAddonVariables.ofOrDefault(player);
+			vars.isTransfuredBySafeMethod = true;
 
+			if (!ftkc) {
+				FightToKeepConsciousness.MinigameType minigameType = FightToKeepConsciousness.MinigameType.getRandom(player.getRandom());
+				vars.isTransfuredBySafeMethod = false;
+				updatePlayerVariables(vars, minigameType, 0, player);
+				vars.syncPlayerVariables(player);
+				ChangedAddonMod.PACKET_HANDLER.send(PacketDistributor.PLAYER.with(() -> player), new ClientboundOpenFTKCScreenPacket(minigameType));
+				return;
+			}
+			vars.syncPlayerVariables(player);
 		}
 	}
 
-	public static boolean handleCaddonCfg(ProcessTransfur.KeepConsciousEvent event) {
-		ChangedAddonVariables.PlayerVariables vars = ChangedAddonVariables.ofOrDefault(event.player);
-
-		vars.isTransfuredBySafeMethod = true;
-		vars.syncPlayerVariables(event.player);
-
-		boolean ftkc = event.player.level().getGameRules().getBoolean(ChangedAddonGameRules.FIGHT_TO_KEEP_CONSCIOUSNESS);
-
-		if(ftkc && event.player instanceof ServerPlayer serverPlayer) {
-			successFTKC(vars, serverPlayer);
-			vars.syncPlayerVariables(serverPlayer);
-			return true;
-		}
+	public static boolean handleCaddonCompatCfg(ProcessTransfur.KeepConsciousEvent event) {
 
 		if ( event.player.hasEffect(UNSAFE_SERUM.get()) && UNSAFE_SERUM_CADDON_HANDLING.get() == ServerCfg.CAddonHandleMode.FORCE_SAFE_TF  ) {
 			return true;
@@ -87,26 +82,30 @@ public class EffectTransfurEventHandler {
 	}
 
 
-	@SubscribeEvent
-	public static void onTick(TickEvent.PlayerTickEvent event) {
-		if (ModList.get().isLoaded("changed_addon")) {
-			if (event.phase != TickEvent.Phase.END) {return;}
-			if (!event.player.isAlive()) {return;}
-			if (!(event.player instanceof ServerPlayer player)) {return;}
+	@SubscribeEvent(priority = EventPriority.HIGH)
+	public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase != TickEvent.Phase.END) { return; }
+		if (event.player instanceof ServerPlayer player) {
+			if (!player.isAlive()) { return; }
 
+
+			if (player.level().getGameRules().getBoolean(ChangedAddonGameRules.FIGHT_TO_KEEP_CONSCIOUSNESS) ) {
+				return;
+			}
+
+			TransfurVariantInstance<?> instance = ProcessTransfur.getPlayerTransfurVariant(player);
 			ChangedAddonVariables.PlayerVariables vars = ChangedAddonVariables.ofOrDefault(player);
+			vars.timeAfterVictoryOfFTK = 0;
+			vars.syncPlayerVariables(player);
 
 			if (vars.FTKCminigameType == null) {
 				return;
 			}
 
-			TransfurVariantInstance<?> instance = ProcessTransfur.getPlayerTransfurVariant(player);
-
 			if (instance == null) {
 				successFTKC(vars, player);
 				return;
 			}
-
 			++vars.ticksFightingForConsciousness;
 			vars.syncPlayerVariables(player);
 			if (vars.ticksFightingForConsciousness >= getStruggleTime()) {
